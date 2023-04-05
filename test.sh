04c97c6eb3e63bf6588bb2e7732219c8ee75821c
AjeeshPhilip <ajeesh.philip@sky.uk>
2023-04-05 16:02:26 +0530

    files

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+REPO_URL="https://github.com/AjeeshPhilip/test.git"
+
+PPK_FILE="C:\Users\apl42\.gcp_putty_ssh\id_rsa.ppk" 
+
+USERNAME="ajeesh_philip_sky_uk"
+IP_ADDRESS="10.53.88.219"
+
+REMOTE_PATH="/home/ajeesh_philip_sky_uk/test"
+
+MYREPO='test'
+
+git clone $REPO_URL
+
+pscp -i $PPK_FILE -r $MYREPO $USERNAME@$IP_ADDRESS:$REMOTE_PATH
