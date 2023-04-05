c82207a70cb4666765b8def5aa3bbd5b9e3567da
AjeeshPhilip <116163526+AjeeshPhilip@users.noreply.github.com>
2023-04-05 16:09:58 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 REPO_URL="https://github.com/AjeeshPhilip/test.git"
 
-PPK_FILE="C:\Users\apl42\.gcp_putty_ssh\id_rsa.ppk" 
+PPK_FILE="C:/Users/apl42/.gcp_putty_ssh/id_rsa.ppk" 
 
 USERNAME="ajeesh_philip_sky_uk"
 IP_ADDRESS="10.53.88.219"
